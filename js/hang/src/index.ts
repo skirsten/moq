cc64ae4bb73a8a60bfffd8fa9dae10428d3350a6
Simon Kirsten <simonkirsten24@gmail.com>
2025-09-24 14:57:58 +0200

    Add missing time export

--- js/hang/src/index.ts
+++ js/hang/src/index.ts
@@ -1,6 +1,7 @@
 export * as Moq from "@kixelated/moq";
 export * as Catalog from "./catalog";
 export * as Frame from "./frame";
+export * as Time from "./time";
 export * as Publish from "./publish";
 export * as Support from "./support";
 export * as Watch from "./watch";
