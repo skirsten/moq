cf8f88385d8d5744a4d315148f3e1870b420d237
Luke Curley <kixelated@gmail.com>
2026-02-27 11:06:36 -0800

    Pin the deployed moq-relay version.

--- cdn/flake.nix
+++ cdn/flake.nix
@@ -6,7 +6,7 @@
     moq = {
       # Unfortunately, we can't use a relative path here because it executes on the remote.
       # TODO cross-compile locally and upload the binary to the remote.
-      url = "github:moq-dev/moq";
+      url = "github:moq-dev/moq/moq-relay-v0.10.5";
     };
   };
 
@@ -29,7 +29,7 @@
           moq-relay = moq.packages.${system}.moq-relay;
           cachix = pkgs.cachix;
           ffmpeg = pkgs.ffmpeg;
-          hang-cli = moq.packages.${system}.hang;
+          moq-cli = moq.packages.${system}.moq-cli;
         };
     };
 }
