bbe61c480fd0992188fae37a328f7033af1a89ad
Luke Curley <kixelated@gmail.com>
2026-04-03 21:26:40 -0700

    Not needed lul

--- cdn/pub/flake.nix
+++ cdn/pub/flake.nix
@@ -1,11 +1,6 @@
 {
   description = "MoQ publisher dependencies";
 
-  nixConfig = {
-    extra-substituters = [ "https://kixelated.cachix.org" ];
-    extra-trusted-public-keys = [ "kixelated.cachix.org-1:CmFcV0lyM6KuVM2m9mih0q4SrAa0XyCsiM7GHrz3KKk=" ];
-  };
-
   inputs = {
     # Pin to a release tag via: just pin
     moq.url = "github:moq-dev/moq";
